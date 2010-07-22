0e5fc1b56dd2cfcf0e16ab3c550b4ec5dea906fe
Deflis <deflis@gmail.com>
2010-07-23 02:35:59 +0900

    AsOrderedを追加。

--- NekoVampire.MathLib/Prime.cs
+++ NekoVampire.MathLib/Prime.cs
@@ -38,7 +38,7 @@ namespace NekoVampire.MathLib
         public IEnumerable<long> Primes()
         {
             var copy = primes.ToArray();
-            return copy.Concat(max.UpToInfinity().Where(IsPrime));
+            return copy.Concat(max.UpToInfinity().AsParallel().AsOrdered().Where(IsPrime));
         }
     }
 }
