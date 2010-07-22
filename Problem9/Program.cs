d4df330c7813987423720efe1edbc969334b7cb8
Deflis <deflis@gmail.com>
2010-07-23 03:14:05 +0900

    Problem9を解いた。

---
+++ Problem9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Achiral.Extension;
+
+namespace Problem9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}",
+                1.UpTo(998).SelectMany(
+                a =>
+                    (a + 1).UpTo(999).Where(
+                        b =>
+                            Math.Pow(1000 - a - b, 2) == Math.Pow(a, 2) + Math.Pow(b, 2)
+                    ).Select(
+                        b =>
+                            a * b * (1000 - a - b)
+                        )
+                ).Single());
+            Console.ReadKey();
+        }
+    }
+}
