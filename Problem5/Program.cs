33a52205cc7b101ba042105d02d59a46af36510b
Deflis <deflis@gmail.com>
2010-07-23 01:32:14 +0900

    Problem5が解けた。

---
+++ Problem5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Achiral.Extension;
+
+namespace Problem5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 2520.UpToInfinity();
+            Console.WriteLine("{0}", n.AsParallel().Where(
+                x =>
+                    ! 2.UpTo(20)
+                    .Any(y =>
+                        x % y != 0)
+                    ).First());
+            Console.ReadKey(); 
+        }
+    }
+}
